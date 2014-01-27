4ee5118ea2325f94726de315491c13be83e368f2
Stephen Best <bestie@gmail.com>
2014-01-27 13:11:57 +0000

    Added failing test for second Fibonacci number

--- src/test/scala/TestFibonacci.scala
+++ src/test/scala/TestFibonacci.scala
@@ -2,9 +2,20 @@ import org.scalatest._
 
 class TestFibonacci extends FunSpec with ShouldMatchers {
 
-  describe("A Stack") {
-    it("should pop values in last-in-first-out order") {
-      1 should be(1)
+  describe("A Fibonacci sequence") {
+    it("should have zero as its first number") {
+      val fibonacci = new Fibonacci
+      fibonacci.getNumber(0) should be(0)
     }
+    it("should have one as its second number") {
+      val fibonacci = new Fibonacci
+      fibonacci.getNumber(1) should be(1)
+    }
+  }
+}
+
+class Fibonacci {
+  def getNumber(index: Int) : Int = {
+    0
   }
 }
