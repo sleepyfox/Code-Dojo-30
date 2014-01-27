58652dda3f0424c7538989d36630699ca38bf81e
Stephen Best <bestie@gmail.com>
2014-01-27 12:55:25 +0000

    First spec to test SBT working

--- src/test/scala/TestFibonacci.scala
+++ src/test/scala/TestFibonacci.scala
@@ -2,4 +2,9 @@ import org.scalatest._
 
 class TestFibonacci extends FunSpec with ShouldMatchers {
 
+  describe("A Stack") {
+    it("should pop values in last-in-first-out order") {
+      1 should be(1)
+    }
+  }
 }
